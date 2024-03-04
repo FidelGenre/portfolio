11724977145afad26360f4edc633dd884f7ed614
Fidel Genre <trabajosfidel4@gmail.com>
2024-03-04 16:33:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -99,14 +99,43 @@ function descargarArchivo() {
 
 /*cv*/
 
-function sendEmail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
-    }
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-    emailjs.send("service_wyl2d47","template_3cg6973",parms).then(alert("Email Sent"))
-}
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
 
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
 
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
